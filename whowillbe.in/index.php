88b31668d6c7a2c23d4cbc5f1215ba70953c154c
Makinfosys <piyushneoweb@gmail.com>
2019-02-07 14:51:47 +0530

    First commit

---
+++ whowillbe.in/index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>WhoWillBe.in</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style-3dflip.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+</head>
+<body style="background:transparent url('img/india-map.jpg') no-repeat center center/cover; margin-top: 0px;">  
+  <div class="container-fluid">  
+    <div class="component">
+      <div class="row">
+        <div class="col-sm-8">
+          <h1 style="font-size:5vw; color: #E5C55B; text-shadow: 1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue;" class="typping">Who Will Be Next PM?</h1>          
+        </div>
+<!--         <div class="col-sm-4">
+          <div class="cards">
+            <div class="cards__container">
+              <div class="cards__front card-1"></div>
+              <div class="cards__back card-2"></div>
+            </div>
+          </div>
+        </div>
+ -->      </div>
+  
+      <!-- Start Nav Structure -->
+        <button class="cn-button" style="background:transparent url('img/ac.png') no-repeat center center/cover; color: Red;" id="cn-button">Menu</button>
+        <div class="cn-wrapper" id="cn-wrapper">
+          <ul>
+            <li>
+              <a href="#">
+                <span>Home</span>
+                <svg class="caticon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                     width="35px" height="70px" viewBox="0 0 512 512" enable-background="new 0 0 50 50" xml:space="preserve">
+                  <path fill="white" id="shop-3-icon" d="M79.792,217.25v235h352.75v-235H79.792z M397.542,381.75h-282.75v-129.5h282.75V381.75z M220.62,59.75
+                                            l-17.798,85.332h-0.081v17.34c0,18.314-14.847,33.161-33.161,33.161s-33.16-14.847-33.16-33.161v-17.34l50.841-85.332H220.62z
+                                            M168.232,59.75l-51.91,85.332v17.34c0,18.314-14.847,33.161-33.161,33.161S50,180.736,50,162.422v-17.34l83.666-85.332H168.232z
+                                            M462,145.082v17.34c0,18.314-14.847,33.161-33.161,33.161s-33.161-14.847-33.161-33.161v-17.34l-51.91-85.332h34.566L462,145.082z
+                                            M289.08,145.082h0.081v17.34c0,18.314-14.847,33.161-33.161,33.161s-33.161-14.847-33.161-33.161v-17.34h0.081l16.729-85.332
+                                            h32.703L289.08,145.082z M324.739,59.75l50.841,85.332v17.34c0,18.314-14.846,33.161-33.16,33.161s-33.161-14.847-33.161-33.161
+                                            v-17.34h-0.081L291.38,59.75H324.739z"/>
+                </svg>
+              </a>
+            </li>
+            <li><a href="#"><span>Furniture</span></a></li>
+            <li><a href="#"><span>Transport</span></a></li>
+            <li><a href="#"><span>Gift</span></a></li>
+            <li><a href="#"><span>Clothes</span></a></li>
+            <li><a href="#"><span>Games</span></a></li>
+            <li><a href="#"><span>Stationary</span></a></li>
+            <li><a href="#"><span>Toys</span></a></li>
+            <li><a href="#"><span>Books</span></a></li>
+            <li><a href="#"><span>Login</span></a></li>
+            <li><a href="#"><span>Register</span></a></li>
+            <li><a href="#"><span>Sell</span></a></li>
+          </ul>
+        </div>
+        <!-- End of Nav Structure -->
+        <div class="row" >
+          <div class="col-lg-1" style="background-color:black;">
+            <div style="background-color: red; float: left; width: 200px; margin-left: -15px; border-radius: 5px; border-color: black; display: inline-block;">
+            News Flash :  
+            </div>
+          </div>
+          <div class="col-lg-11" style="background-color:black;">
+              <marquee> Hey</marquee>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-2" style="background-color:none; color: black;">
+            PM Lit Slider Will Be here
+            PM Lit Slider Will Be here
+            PM Lit Slider Will Be here
+            PM Lit Slider Will Be here
+            PM Lit Slider Will Be here
+            PM Lit Slider Will Be here
+            PM Lit Slider Will Be here
+          </div>
+
+          <div class="col-lg-8" style="background-color:none; color: black; padding-top: 130px;">
+           <div class="col-lg">
+            <table class="table table-responsive table-bordered">
+              <tbody>
+                <tr>
+                   <td>Jharkhand</td>
+                   <td>Karnataka</td>
+                   <td>Kerala</td>
+                   <td>MP</td>
+                   <td>Maharashtra</td>
+                   <td>Manipur</td>
+                   <td>Meghalaya</td>
+                   <td>Mizoram</td>
+                </tr>
+                <tr>
+                   <td>AP</td>
+                   <td colspan="6" rowspan="8">State Data will be Desplyed Here</td>
+                   <td>Nagaland</td>
+                </tr>
+                <tr>
+                  <td>ARP</td>
+                  <td>Orissa</td>
+                </tr>
+                <tr>
+                  <td>Bihar</td>
+                  <td>Rajasthan</td>
+                </tr>
+                <tr>
+                  <td>CH</td>
+                  <td>Sikkim</td>
+                </tr>
+                <tr>
+                  <td>Goa</td>
+                  <td>Tamil Nadu</td>
+                </tr>
+                <tr>
+                  <td>Gujarat</td>
+                  <td>Telagana</td>
+                </tr>
+                <tr>
+                  <td>Haryana</td>
+                  <td>Tripura</td>
+                </tr>
+                <tr>
+                  <td>HP</td>
+                  <td>Uttaranchal</td>
+                </tr>
+                <tr>
+                  <td>JK</td>
+                  <td>UP</td>
+                  <td>West Bengal</td>
+                  <td>AN</td>
+                  <td>Chandigarh</td>
+                  <td>DN</td>
+                  <td>Daman and Diu</td>
+                  <td>Delhi</td>
+                </tr>
+              </tbody>
+            </table>
+            </div>
+          </div>
+          <div class="col-lg-2" style="background-color:none; color: black;">
+              Here will be the list of National Parties
+              Here will be the list of National Parties
+              Here will be the list of National Parties
+              Here will be the list of National Parties
+              Here will be the list of National Parties
+              Here will be the list of National Parties
+              Here will be the list of National Parties
+          </div>
+        </div>
+
+        </div>
+  </div>
+  <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+  <script  src="js/index.js"></script>
+  <script  src="js/index-3dflip.js"></script>
+</body>
+</html>
---
+++ whowillbe.in/js/index-3dflip.js
@@ -0,0 +1,70 @@
+// You can change the speed and amount of cards here
+var cardAmount = 4;
+var flipSpeed = 1500;
+
+// Preload all images to prevent blank cards
+// because they're switched with CSS classes
+(function preload(imageArray) {
+    $(imageArray).each(function(){
+        $('<img/>')[0].src = this;
+    });
+})(["img/leaders/rahul.jpeg",
+	"img/leaders/rahul.jpeg",
+	"img/leaders/rahul.jpeg",
+	"img/leaders/rahul.jpeg"]);
+
+// 3D flip slideshow
+// 'https://source.unsplash.com/YQ3FpeQkNhA/800x600',
+// 	'https://source.unsplash.com/gGC63oug3iY/800x600',
+// 	'https://source.unsplash.com/PAcI-vmFL2g/800x600',
+// 	'https://source.unsplash.com/_d0zgyMmYT8/800x600'
+(function bfCards(elements, speed) {
+	var cards = $('.cards');
+	var container = cards.children('.cards__container');
+	var front = container.children('.cards__front');
+	var back = container.children('.cards__back');
+
+	function swapArticleClass(element, newClass) {
+		element.removeClass(function(index, css) {
+			return (css.match(/(^|\s)card-\S+/g) || []).join(' ');
+		});
+
+		element.addClass(newClass);
+	}
+
+	var onTick = function(i) {
+		setTimeout(function() {
+			var nextClass;
+			var currentClass = 'card-' + i;
+
+			if (currentClass === elements)  {
+				nextClass = 'card-' + (i + 1);
+			} else {
+				nextClass = 'card-1';
+			}
+
+			if (i % 2 === 0) {
+				cards.addClass('is-flipped');
+				swapArticleClass(back, currentClass);
+				setTimeout(function() {
+					swapArticleClass(front, nextClass);
+				}, speed / 2);
+			} else {
+				cards.removeClass('is-flipped');
+				swapArticleClass(front, currentClass);
+				setTimeout(function() {
+					swapArticleClass(back, nextClass);
+				}, speed / 2);
+			}
+		}, speed * i);
+	};
+
+	function cycle() {
+		for (var i = 1; i <= elements; i++) {
+			onTick(i);
+		}
+	};
+
+	cycle();
+	setInterval(cycle, speed * elements);
+})(cardAmount, flipSpeed);
